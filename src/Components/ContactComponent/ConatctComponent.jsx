a5cc7930b83b362a4133c9a1c188cea592bb912b
harshad <Harshadkajale333@gmail.com>
2023-12-17 20:20:44 +0530

    code updated

--- src/Components/ContactComponent/ConatctComponent.jsx
+++ src/Components/ContactComponent/ConatctComponent.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './ConatctComponent.css'
-function ConatctComponent() {
+import emailjs from "@emailjs/browser";
 
+function ConatctComponent() {
+    
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      // Replace with your actual EmailJS service ID, template ID, and public key
+      const serviceId = "service_gmre1gr";
+      const templateId = "template_ijqtu8v";
+      const publicKey = "Lwa5WZvrG1rdoAJnH";
+  
+      emailjs.sendForm(serviceId, templateId, form.current, publicKey).then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    };
+  
     return (
         <div className='contact-fullcontainer'>
             <div className='Contcat-right'>
                 
-                <form className="contact-us-form">
+                <form ref={form}  onSubmit={sendEmail} className="contact-us-form">
                     <h1>We're Ready to Work with You.<br/> Get in Touch</h1>
     <label htmlFor="name">Name</label>
-    <input type="text" id="name" name="name" required />
+    <input type="text" id="name" name="from_name" required />
 
     <label htmlFor="email">Email</label>
-    <input type="email" id="email" name="email" required />
+    <input type="email" id="email" name="reply_to" required />
 
     <label htmlFor="message">Message</label>
     <textarea id="message" name="message" required />
 
-    <button type="submit">Submit</button>
+    <button  type="submit">Submit</button>
                 </form>
             </div>
             <div className='contact-left'>
